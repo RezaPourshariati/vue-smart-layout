8fe5cccc078a79a9c0e618a9f0219af5508674bb
Reza Pourshariati <rezaporshariati@gmail.com>
2025-01-11 17:52:16 +0330

    fix(client): fixed some styles in main.css

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import AppLayout from "@/layouts/AppLayout.vue"
 </script>
 
 <template>
--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,11 +1,11 @@
 import { defineAsyncComponent } from 'vue'
 
-export const layoutMap = {
+export const layoutMap: any = {
   AppLayoutDefault: defineAsyncComponent(() => import('./AppLayoutDefault.vue')),
   AppLayoutHome: defineAsyncComponent(() => import('./AppLayoutHome.vue')),
   AppLayoutAbout: defineAsyncComponent(() => import('./AppLayoutAbout.vue')),
   AppLayoutContacts: defineAsyncComponent(() => import('./AppLayoutContacts.vue')),
 }
 
-export const defaultLayout = 'AppLayoutDefault'
+export const defaultLayout: string = 'AppLayoutDefault'
 
