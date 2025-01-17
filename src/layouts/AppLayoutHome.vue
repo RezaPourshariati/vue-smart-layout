c0d7d2b4d6768c0d41d2c9175cd9470e7d215725
Reza Pourshariati <rezaporshariati@gmail.com>
2025-01-17 23:10:14 +0330

    fix(client): changed some styles in app layout

--- src/layouts/AppLayoutHome.vue
+++ src/layouts/AppLayoutHome.vue
@@ -3,14 +3,31 @@ import AppLayoutLinks from "./AppLayoutLinks.vue"
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <header class="header" />
     <AppLayoutLinks />
-    <slot />
+    <div class="home-page-layout">
+      <slot />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+}
+
+.home-page-layout {
+  width: 100%;
+  max-width: 1100px;
+  border: 2px solid red;
+  margin: 0 2rem;
+  height: 100vh;
+}
+
 .header {
   height: 5rem;
   background-color: green;
