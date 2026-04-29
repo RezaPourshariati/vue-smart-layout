4d2515e30443cfaf54bc6ce15241f4f8554cf6ae
رضا پورشریعتی <rpourshariati@noornent.net>
2026-04-29 13:12:53 +0330

    fix(client): fixed user.model.ts file

--- back-end/src/models/user.model.ts
+++ back-end/src/models/user.model.ts
@@ -54,13 +54,12 @@ const userSchema = new Schema<IUser>(
   { timestamps: true, minimize: false },
 )
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password'))
-    return next()
+    return
 
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
-  next()
 })
 
 const User = mongoose.model<IUser>('User', userSchema)
